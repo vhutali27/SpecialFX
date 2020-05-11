40eaa499202c0797bddd1ece386e256415366678
1715260 <ubuntu@DESKTOP-LNAKLHL.localdomain>
2020-05-11 02:51:33 +0200

    added document body to PointerLockControls constructor as well as commented out line 330 to line 349

--- js/earth.js
+++ js/earth.js
@@ -55,7 +55,7 @@ class Player{
 		// First Person Camera and Controls
 		// init_player
 		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000);
-		this.controls = new THREE.PointerLockControls( this.camera );
+		this.controls = new THREE.PointerLockControls( this.camera, document.body); // Not sure whi
 
 		// Raycasting is used for working out which object the mouse is pointing at
 		this.raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
@@ -327,26 +327,26 @@ if ( havePointerLock ) {
 	document.addEventListener( 'pointerlockerror', pointerlockerror, false );
 	document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
 	document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
-	instructions.addEventListener( 'click', function ( event ) {
-		instructions.style.display = 'none';
-		// Ask the browser to lock the pointer
-		element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
-		if ( /Firefox/i.test( navigator.userAgent ) ) {
-			var fullscreenchange = function ( event ) {
-				if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
-					document.removeEventListener( 'fullscreenchange', fullscreenchange );
-					document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
-					element.requestPointerLock();
-				}
-			};
-			document.addEventListener( 'fullscreenchange', fullscreenchange, false );
-			document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
-			element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
-			element.requestFullscreen();
-		} else {
-			element.requestPointerLock();
-		}
-	}, false );
+	// instructions.addEventListener( 'click', function ( event ) {
+	// 	instructions.style.display = 'none';
+	// 	// Ask the browser to lock the pointer
+	// 	element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+	// 	if ( /Firefox/i.test( navigator.userAgent ) ) {
+	// 		var fullscreenchange = function ( event ) {
+	// 			if ( document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element ) {
+	// 				document.removeEventListener( 'fullscreenchange', fullscreenchange );
+	// 				document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+	// 				element.requestPointerLock();
+	// 			}
+	// 		};
+	// 		document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+	// 		document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+	// 		element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+	// 		element.requestFullscreen();
+	// 	} else {
+	// 		element.requestPointerLock();
+	// 	}
+	// }, false );
 } else {
 	instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
 }
