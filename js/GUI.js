f289476b57d99bc5af6af06e2f67112c750879a6
vhutali27 <vhutali.tshavhungwe@outlook.com>
2020-06-12 15:57:36 +0200

    added icons to the health and energy bar, as well as polished the interface, so that all GUI elements only load when the game starts

--- js/GUI.js
+++ js/GUI.js
@@ -1,9 +1,4 @@
-var healthBar;
-
-function energyBar(){
-    var energyBar_ = document.getElementById("energybar")
-    energyBar_.style.scale(0.1, 0.1)
-}
+var healthBar, energyBar;
 
 function initCountdown(e) {
     var sec, min, hund, secExt, hundExt, setTimer;
@@ -53,22 +48,33 @@ function initCountdown(e) {
 
 function  initGUIElements() {
 
-  /////////////////////////////////////////////////////////
-  // GUI                                                 //
-  /////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+    // GUI                                                 //
+    /////////////////////////////////////////////////////////
 
-  // Display the HUD: radar, health, score, and credits/directions
-  /*$('body').append('<canvas id="radar" width="200" height="200"></canvas>');
-  $('body').append('<div id="hud"><p>Health: <span id="health">100</span><br />Score: <span id="score">0</span></p></div>');
-  // Set up the brief red flash that shows when you get hurt
-  $('body').append('<div id="hurt"></div>');
-  $('#hurt').css({width: window.innerWidth, height: window.innerHeight,});*/
+    // Display the HUD: radar, health, score, and credits/directions
+    /*$('body').append('<canvas id="radar" width="200" height="200"></canvas>');
+    $('body').append('<div id="hud"><p>Health: <span id="health">100</span><br />Score: <span id="score">0</span></p></div>');
+    // Set up the brief red flash that shows when you get hurt
+    $('body').append('<div id="hurt"></div>');
+    $('#hurt').css({width: window.innerWidth, height: window.innerHeight,});*/
 
     // The canvas used to render onto
-    var displayCanvasContext = document.getElementById('displayCanvas').getContext('2d');
-    healthBar = new HealthBar(displayCanvasContext, 5, 5, 50, 5, 100, 100);
+    var countdownDiv = document.getElementById("divCountdown")
+    countdownDiv.style.width = "100px";
+    countdownDiv.style.height = "50px"
 
-    // Initialize the timer
-    initCountdown(180);
-    energyBar();
+    var displayCanvasContext = document.getElementById('displayCanvas').getContext('2d');
+    healthBar = new HealthBar(displayCanvasContext, 15, 1, 50, 5, 100, 100);
+    var healthBarImage = document.getElementById("healthIcon")
+    healthBarImage.style.width = "50px";
+    healthBarImage.style.height = "25px";
+
+    energyBar = new HealthBar(displayCanvasContext, 100, 1, 50, 5, 100, 100);
+    var energyBarImage = document.getElementById("canisterIcon")
+    energyBarImage.style.position = "absolute";
+    energyBarImage.style.width = "50px";
+    energyBarImage.style.height = "25px";
+    energyBarImage.style.left = "350px";
+
 }
